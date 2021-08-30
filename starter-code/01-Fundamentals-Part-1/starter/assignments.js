b12f74d660e942548d16c0a76ba837f05a9896cc
Mhossain308 <Mz.hossain212@gmail.com>
2021-08-30 16:35:25 -0400

    work in progress

---
+++ starter-code/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+/*
+let country = "america";
+
+let continent = 'North America';
+
+let population = 200;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
---
+++ starter-code/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,50 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+// variable name conventions 
+let firstName = 'Matilda';
+console.log(firstName);
+
+//Data types
+-------------------------------------------------
+//boolean
+let javascriptIsFun = true; 
+
+//type of 
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+//dynamic typing 
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+//undefined 
+let year;
+console.log(year)
+
+
+//Let const and var 
+
+let age = 30; 
+age = 31;
+
+// use const most of the time 
+const birthYear =1991;
+// birthYear = 1990;
+//const job;
+
+//dont use var
+var job = 'programmer';
+job = 'teacher';
+
+//bad practice 
+lastName = 'Hossain';
+console.log(lastName);
+
+*/
+
+// Operator practice 
+
+
