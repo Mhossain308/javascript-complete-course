9078e27bae364043f946c0decb66fea4e0298597
Mhossain308 <Mz.hossain212@gmail.com>
2021-09-02 11:45:26 -0400

    coding challenge 2 done

--- starter-code/01-Fundamentals-Part-1/starter/assignments.js
+++ starter-code/01-Fundamentals-Part-1/starter/assignments.js
@@ -8,4 +8,45 @@ let population = 200;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// code challenge #1 
+
+ // 1. store johns and marks mass and height in variables 
+
+ const marksMass = 78; 
+ const johnsMass = 92;
+
+ const marksHeight = 1.69;  
+ const johnsHeight = 1.95;
+
+
+ console.log(marksMass, johnsMass);
+ console.log(marksHeight, johnsHeight);
+
+
+
+// 2. calculate there bmis formula BMI = mass / height ** 2 
+
+const marksBmi = marksMass / marksHeight ** 2;
+const johnsBmi = johnsMass / johnsHeight ** 2;
+
+console.log(marksBmi)
+console.log(johnsBmi)
+
+
+
+// 3. create a boolean variable marksHigherBMI
+
+const marksHigherBMI = marksBmi > johnsBmi;
+console.log(marksHigherBMI)
+
+// coding challenge #2
+
+// print a output to console saying who has the higher bmi "Marks BMI is higher than johns "
+
+if(marksBmi > johnsBmi) { 
+   console.log(`Marks bmi (${marksBmi}) is higher then Johns BMI (${johnsBmi})!`)
+}  else { 
+console.log(`Johns Bmi ${johnsBmi} is higher then Marks Bmi ${marksBmi}!`)
+};
 */
--- starter-code/01-Fundamentals-Part-1/starter/script.js
+++ starter-code/01-Fundamentals-Part-1/starter/script.js
@@ -43,8 +43,121 @@ job = 'teacher';
 lastName = 'Hossain';
 console.log(lastName);
 
-*/
+
 
 // Operator practice 
+const now = 2037
+const ageJonas = 2037 - 1991;
+const ageSarah = now -2018
+console.log(ageJonas);
+console.log(ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+// "**" is exponents
+
+const firstName = "Mohamed"
+const lastName = "Hossain"
+console.log(firstName + ' ' + lastName)
+
+// assignment operaters 
+
+let x = 10 + 5; // 15
+console.log(x)
+
+x += 10; // x= x +10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+
+//Comparison operators 
+
+console.log(ageJonas > ageSarah) // >, <, >=. <=
+console.log(ageSarah >= 18)
+
+console.log(now - 1991 > now - 2018);
+
+let m, y;
+m = y = 25 - 10 -5;
+console.log(m,y);
+
+
+// strings and template literals 
+
+const firstName =  'Mohamed';
+const job = 'progammer';
+const birthYear = 1995;
+const year = 2021;
+
+const mohamed = "I'm " + firstName + ', a ' +  (year - birthYear) + ' years old ' + job + '!';
+console.log(mohamed);
+//  template string using string interpolation Es6
+const mohamedNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+console.log(mohamedNew)
+
+// multi line string
+
+console.log(`string
+multiple
+lines`);
+
+*/
+
+
+// if else statements 
+const age = 15;
+
+const isOldEnough =  age >= 18;
+
+if(age >= 18) {
+    console.log('Sarah can start driving ')
+
+} else {
+    const yearsLeft = 18 - age; 
+//    console.log(`Sarah is too young. wait another ${yearsLeft} years`)
+}
+
+
+const birthYear = 2015;
+
+let century; 
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+// console.log(century)
+
+// type conversion and coercion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
