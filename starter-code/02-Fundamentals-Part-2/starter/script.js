ccfd22f52943543758b5522e0e8d2dd6a92f63fb
Mhossain308 <Mz.hossain212@gmail.com>
2021-09-27 14:38:20 -0400

    updated

--- starter-code/02-Fundamentals-Part-2/starter/script.js
+++ starter-code/02-Fundamentals-Part-2/starter/script.js
@@ -35,17 +35,39 @@ console.log(appleOrangeJuice)
 
 // function declaration vs expressions
 // function declaration 
+
 function calAge1(birthyear) {
     return 2037 - birthyear
 }
 
-
 const age1 = calAge1(1991)
+
 console.log(age1)
 
 // function expression
- const calcAge2 =  function (birthYear) {
-    return 2037 - birthYear
+//  const calcAge2 =  function (birthYear) {
+//     return 2037 - birthYear
+// }
+// const age2 =  calcAge2(1991);
+
+//  console.log(age1, age2);
+
+// arrow functions 
+
+
+
+// arrow functions 
+const calcAge3 =  birthYear => 2037 - birthYear
+const age3 = calcAge3(1991);
+console.log(age3)
+
+
+
+const yearsUntillRetirement = (birthYear, firstName) => {
+    const age =  2037 - birthYear
+    const retirement = 65 - age
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
 }
-const age2 =  calcAge2(1991)
-console.log(age2)
+const retire = yearsUntillRetirement(1991, 'Mohamed')
+console.log(retire)
